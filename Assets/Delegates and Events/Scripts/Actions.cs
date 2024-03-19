0303b036896f5a4a9a9ca4f99b93329bd9aeeb50
Chaitanya Estarla <35031758+ChaitanyaEstarla@users.noreply.github.com>
2024-03-19 18:00:17 +0530

    Events and Delegates
    added Event and an async example in delegates

--- Assets/Delegates and Events/Scripts/Actions.cs
+++ Assets/Delegates and Events/Scripts/Actions.cs
@@ -1,34 +1,36 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Events;
-using System;
+using UnityEngine.UI;
 
-public class Actions : MonoBehaviour
+namespace Delegates_and_Events.Scripts
 {
-    private Button _mAddButton;
-    private Renderer _mRenderer;
-    private UnityAction _mMyFirstAction;
-    //This is the number that the script updates
-    private float _mMyNumber; 
+    public class Actions : MonoBehaviour
+    {
+        private Button _mAddButton;
+        private Image _mImage;
+        private UnityAction _mMyFirstAction;
+        //This is the number that the script updates
+        private float _mMyNumber; 
     
-    private void Start()
-    {
-        _mAddButton = GetComponent<Button>();
-        _mRenderer = GetComponent<SpriteRenderer>();
-        //Make a Unity Action that calls your function
-        _mMyFirstAction += MyFunction;
-        _mMyFirstAction += MySecondFunction;
-        //Register the Button to detect clicks and call your Unity Action
-        _mAddButton.onClick.AddListener(_mMyFirstAction);
-    }
-    private void MyFunction()
-    {
-        _mMyNumber++;
-        Debug.Log("First Added : " + _mMyNumber);
-    } 
-    private void MySecondFunction()
-    {
-        _mRenderer.material.color = Color.blue;
-        Debug.Log("Second Added");
+        private void Start()
+        {
+            _mAddButton = GetComponent<Button>();
+            _mImage = GetComponent<Image>();
+            //Make a Unity Action that calls your function
+            _mMyFirstAction += MyFunction;
+            _mMyFirstAction += MySecondFunction;
+            //Register the Button to detect clicks and call your Unity Action
+            _mAddButton.onClick.AddListener(_mMyFirstAction);
+        }
+        private void MyFunction()
+        {
+            _mMyNumber++;
+            Debug.Log("First Added : " + _mMyNumber);
+        } 
+        private void MySecondFunction()
+        {
+            _mImage.color = Color.blue;
+            Debug.Log("Second Added");
+        }
     }
 }
--- Assets/Delegates and Events/Scripts/Delegates.cs
+++ Assets/Delegates and Events/Scripts/Delegates.cs
@@ -1,29 +1,30 @@
+using System;
 using UnityEngine;
+using System.Threading.Tasks;
 
 namespace Delegates_and_Events.Scripts
 {
     public class Delegates : MonoBehaviour
     {
-        delegate void PointerToMath(int num1, int num2);
-
+        private delegate void PointerToMath(int num1, int num2);
         private PointerToMath _mathFunctions;
+        private event PointerToMath MathCalcEvents;
 
         #region Untiy Events
 
         private void OnEnable()
         {
+            _mathFunctions = DebugParams;
             _mathFunctions += Add;
             _mathFunctions += Sub;
             _mathFunctions += Mul;
             _mathFunctions += Div;
+            MathCalcEvents += _mathFunctions;
         }
 
         private void Start()
         {
-            _mathFunctions += Add;
-            _mathFunctions += Sub;
-            _mathFunctions += Mul;
-            _mathFunctions += Div;
+            //AsyncTaskTest();
         }
 
         private void Update()
@@ -31,6 +32,7 @@ namespace Delegates_and_Events.Scripts
             if (Input.GetKeyUp(KeyCode.A))
             {
                 _mathFunctions(5, 2);
+                //MathCalcEvents?.Invoke(5, 2);
             }
         }
     
@@ -42,34 +44,54 @@ namespace Delegates_and_Events.Scripts
             _mathFunctions -= Div;
         }
     
-        private void OnDestroy()
-        {
-            _mathFunctions -= Add;
-            _mathFunctions -= Sub;
-            _mathFunctions -= Mul;
-            _mathFunctions -= Div;
-        }
-
         #endregion
+        
+        private delegate Task<int> CalculateAsyncDelegate(int a, int b);
 
         #region Member Functions
 
+        private async void AsyncTaskTest()
+        {
+            CalculateAsyncDelegate calculateDelegate = CalculateAsync;
+
+            Debug.Log("Starting calculation...");
+
+            // Invoke the async method using the delegate and await the result
+            var result = await calculateDelegate(10, 20);
+
+            Debug.Log($"Calculation result: {result}");
+        }
+
+        private static async Task<int> CalculateAsync(int a, int b)
+        {
+            // Simulate a time-consuming operation (e.g., complex calculation)
+            await Task.Delay(2000); // Delay for 2 seconds
+
+            var result = a + b;
+            return result;
+        }
+
+        private void DebugParams(int a, int b)
+        {
+            Debug.Log("Params: " + a + " " + b);
+        }
+
         private void Add(int a, int b)
         {
-            Debug.Log(a+b);
+            Debug.Log("Addition: "+(a+b));
         }
     
         private void Sub(int a, int b)
         {
-            Debug.Log(a-b);
+            Debug.Log("Subtraction: " +(a-b));
         }
         private void Mul(int a, int b)
         {
-            Debug.Log(a*b);
+            Debug.Log("Multiplication: " +(a*b));
         }
         private void Div(int a, int b)
         {
-            Debug.Log(a/b);
+            Debug.Log("Division: " +(a/b));
         }
 
         #endregion
